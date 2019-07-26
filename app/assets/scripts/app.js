5686610705882b7b3b904900c9acea0899437ec6
Mariola <mola8519@gmail.com>
2019-07-26 07:48:08 +0000

    completed the create-modal section

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,11 +1,12 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/stickyHeader';
+import Modal from './modules/Modal';
 import $ from 'jquery';
 
 const mobileMenu = new MobileMenu();
 /* ponieważ chcę aby elementy pojawiały się w różnym momencie scrollowania dodaje 2 instances klasy RevealOnScroll z dwoma innymi elemetami, trzeba pamiętać aby dodać również jquery na górze, idż do RevealOnScroll.js  */
  new RevealOnScroll($('.feature-item'), '85%');
  new RevealOnScroll($('.testimonial'), '60%');
-
  const stickyHeader = new StickyHeader();
+ const modal = new Modal();
